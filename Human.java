9682e6764732768285528f4a43748a18df8df1f4
sergim1423 <67722057+sergim1423@users.noreply.github.com>
2020-11-09 22:36:11 -0500

    changed human
    different strength and hp ect

--- Human.java
+++ Human.java
@@ -5,15 +5,15 @@
  * Implements a maximum/minimum strength for the creature type [20/5]
  * Implements a maximum/minimum hitpoint total for the creature type [25/5]
  * 
- * @author Bill Crosbie
- * @version 2020-10 v1.0
+ * @author Sergius Manolov
+ * @version 2020-11-9
  */
 public class Human extends Creature
 {
     // instance variables - replace the example below with your own
-    private static final int MAX_HUMAN_HP = 25;
-    private static final int MIN_HUMAN_HP = 5;
-    private static final int MAX_HUMAN_STR = 20;
+    private static final int MAX_HUMAN_HP = 30;
+    private static final int MIN_HUMAN_HP = 10;
+    private static final int MAX_HUMAN_STR = 18;
     private static final int MIN_HUMAN_STR = 5;
 
     /**
