16658669d4e5bac53a8c3a20fbe94e5c73130853
sergim1423 <67722057+sergim1423@users.noreply.github.com>
2020-11-09 22:58:48 -0500

    changed elf stats
    extended elf to creature

--- Elf.java
+++ Elf.java
@@ -1,33 +1,40 @@
 
+
 /**
  * Write a description of class Elf here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * 
+ * @author Sergius Manolov 
+ * @version 11/9/20
  */
-public class Elf
+public class Elf extends Creature
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private static final int MAX_ELF_HP = 25;
+    private static final int MIN_ELF_HP = 8;
+    private static final int MAX_ELF_STR = 18;
+    private static final int MIN_ELF_STR = 5;
 
     /**
-     * Constructor for objects of class Elf
+     * Constructor for objects of class ELF
      */
     public Elf()
     {
-        // initialise instance variables
-        x = 0;
+        super(
+            Randomizer.nextInt(MAX_ELF_STR-MIN_ELF_STR)+MIN_ELF_STR,
+            Randomizer.nextInt(MAX_ELF_HP-MIN_ELF_HP)+MIN_ELF_HP
+        );
+          
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public int damage() {
+           int tempdamage;
+           tempdamage = super.damage();
+           
+           if (Randomizer.nextInt(10)==0) {
+               System.err.println("Magic!");
+               tempdamage *=2;
+            }
+           
+            return tempdamage;
     }
 }
